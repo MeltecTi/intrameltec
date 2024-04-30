f0d91d57edd75ede126e2a01008d2335db4acdf5
Tomas Andres Aponte Montenegro <taponte@meltec.dom>
2024-04-30 16:14:37 -0500

    Agrega Cambio

--- resources/js/Pages/Admin/Kpis/Components/KpisTable.jsx
+++ resources/js/Pages/Admin/Kpis/Components/KpisTable.jsx
@@ -37,9 +37,9 @@ export default function KpisTable (id) {
                       </TableCell>
                       <TableCell>
                         <Link className='text-white bg-blue-800 px-5 py-2 rounded-lg mx-1 hover:bg-blue-600 transition ease-out' href={route('kpi.reports.show', id)}>Ver</Link>
-                        {
-                          <DeleteButton id={id} />
-                        }
+                        
+                        <DeleteButton id={id} />
+                        
                       </TableCell>
                     </TableRow>
                   )
