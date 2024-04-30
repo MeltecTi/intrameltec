2c1600f87c1349d3da71f6e18ecf7aebecb4626f
Tomas Andres Aponte Montenegro <taponte@meltec.dom>
2024-04-30 12:02:08 -0500

    Agrega Cambio

--- database/migrations/2023_10_02_220006_create_role_has_kpis_table.php
+++ database/migrations/2023_10_02_220006_create_role_has_kpis_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->uuid('UUID_kpi');
             $table->unsignedBigInteger('role_id');
             $table->foreign('role_id')->references('id')->on('roles');
-            $table->foreign('UUID_kpi')->references('id')->on('kpi_reports');
+            $table->foreign('UUID_kpi')->references('id')->on('kpi_reports')->onDelete('cascade');
         });
     }
 
