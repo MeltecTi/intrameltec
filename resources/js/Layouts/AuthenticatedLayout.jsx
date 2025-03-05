f20f22ec413a77bea979ae1a3578134d69866cbf
JohanFR11 <jcuadros@meltec.com.co>
2025-03-05 10:10:08 -0500

    Kpis route

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -78,6 +78,50 @@ export default function Authenticated({ auth, header, children, unreadNotificati
           <SidebarItem icon={<ToolIcon size='32px' color='#395181' />} href={route('auditoria')} text='Auditoria' />
         ) : ''}
 
+        <div
+          className="flex items-center pl-5 py-2 cursor-pointer rounded-md hover:bg-gray-200 "
+          onClick={() => toggleKpiMenu("kpis")}
+        >
+          <ReportIcon size="32px" color="#395181" />
+          <span className="ml-3 px-5 text-gray-500 font-medium rounded-md"> Kpi's</span>
+          <span className=" ml-2">
+            {openMenus["kpis"] ? (
+              <FaChevronUp size={20} color="#395181" />
+            ) : (
+              <FaChevronDown size={20} color="#395181" />
+            )}
+          </span>
+        </div>
+
+        {openMenus["kpis"] && (
+          <div className="[grid-area:aside] max-w-xs">
+            <NavKpis category={2} />
+            <NavKpis category={3} />
+            <NavKpis category={4} />
+            <div
+              className="flex items-center pl-12 py-2 cursor-pointer rounded-md"
+              onClick={() =>
+                window.open(
+                  "https://lookerstudio.google.com/reporting/fbf99b01-23d9-4bdd-bc64-0de1e53025cc/page/FouGE",
+                  "_blank"
+                )
+              }
+            >
+              <li className="relative flex items-center py-2 my-1 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-gray-600 ml-[-0.5]">
+                <a className="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300 w-full flex items-center space-x-2 py-2 px-2 border-none">
+                  <ReportIcon
+                    size="32px"
+                    color="#395181"
+                  />
+                  <span className="overflow-hidden transition-all w-32 ml-3 text-sm font-medium px-5 self-center text-gray-500 hover:text-gray-700">
+                    Estado Cargue KPI's
+                  </span>
+                </a>
+              </li>
+            </div>
+          </div>
+        )}
+
         <NavKpis />
 
       </Sidebar>
