3cd16ee158c4ebace082cd47d094d152c0f470d9
JohanFR11 <jcuadros@meltec.com.co>
2025-03-05 14:55:59 -0500

    SliderBar meta meltec actualizado

--- resources/js/Pages/Home/Components/SideBarMeta.jsx
+++ resources/js/Pages/Home/Components/SideBarMeta.jsx
@@ -1,8 +1,8 @@
 import { priceFormatted } from '@/helpers/priceHelper'
 import { Slider } from '@nextui-org/react'
 
-export default function SidebarMeta ({ kpi }) {
-  const { TargetDeltaPct, TargetValue, CurrentValue, Name, TargetDeltaAbs } = kpi
+export default function SidebarMeta ({ kpi, totalRevenue}) {
+  const { TargetDeltaPct, CurrentValue ,TargetValue, CurrentValueText, Name, TargetDeltaAbs } = kpi
   const marks = [
     {
       value: 0,
@@ -30,11 +30,20 @@ export default function SidebarMeta ({ kpi }) {
   const valueSlider = (100 + parseFloat(TargetDeltaPct)) / 100
   const colorSlider = (valueSlider * 100) >= 50 ? 'warning' : (valueSlider * 100) >= 80 ? 'success' : 'danger'
 
+  const Resultadofinal = (totalRevenue - TargetValue)
+
   return (
     <>
-      <Slider isDisabled size='lg' maxValue={valueMax} minValue={0} aria-label='Meta 2023' defaultValue={parseFloat(CurrentValue)} marks={marks} color={colorSlider} label={Name} formatOptions={{ style: 'currency', currency: 'COP', minimumFractionDigits: 0 }} />
-
-      <h3 className='p-6 text-right'>Faltan: <span className='font-semibold text-red-600'> {priceFormatted({ price: Math.abs(TargetDeltaAbs), currency: 'COP', fractionDigits: 0 })} </span> Para llegar a la meta del año {new Date().getFullYear()}</h3>
+      <Slider isDisabled size='lg' maxValue={valueMax} minValue={0} aria-label='Meta 2023' defaultValue={parseFloat(totalRevenue)} marks={marks} css={{
+          '& .nextui-slider-bar': {
+            backgroundColor: 'rgb(57, 81, 129)', // Cambiar el color de la barra
+          },
+          '& .nextui-slider-thumb': {
+            backgroundColor: 'rgb(57, 81, 129)', // Cambiar el color del thumb
+            borderColor: '#395181', // Cambiar el borde del thumb
+          }
+        }} label={[CurrentValueText, " || " , " Meta propuesta: " ,  new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(TargetValue)]} formatOptions={{ style: 'currency', currency: 'COP', minimumFractionDigits: 0 }} />
+      <h3 className='p-6 text-right'>Faltan: <span className='font-semibold text-red-600'> {priceFormatted({ price: Math.abs(Resultadofinal), currency: 'COP', fractionDigits: 0 })} </span> Para llegar a la meta del año {new Date().getFullYear()}</h3>
     </>
   )
 }
